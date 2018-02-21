207d3143cc8b98ee7e78ad1ab3f2bc71630a5625
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-02-21 13:31:33 -0600

    map-filter-reduce bonus added and other modified

---
+++ bonus/js/map-filter-reduce-bonus.js
@@ -0,0 +1,72 @@
+const fruits = ["cantaloupe", "orange", "date", "elderberry", "ugli fruit", "pineapple"];
+const customers = [
+    {
+        name: "Fred",
+        age: 58,
+        occupation: "Police Officer",
+        noOfPurchases: 4
+    },
+    {
+        name: "Samantha",
+        age: 54,
+        occupation: "Teacher",
+        noOfPurchases: 18
+    },
+    {
+        name: "Charles",
+        age: 38,
+        occupation: "Librarian",
+        noOfPurchases: 9
+    }
+];
+// PROBLEM 1 - create an array of the first letters of each fruit
+
+//USING REDUCE
+// const arrayOfFirstLetterOfFruit = fruits.reduce((acc, fruit) => {
+//     acc.push(fruit[0]);
+//     return acc;
+// }, []);
+// console.log(arrayOfFirstLetterOfFruit);
+
+//USING MAP
+const arrayOfFirstLetterOfFruit = fruits.map(fruit => fruit[0]);
+console.log(arrayOfFirstLetterOfFruit);
+
+
+// PROBLEM 2 - create array of user objects based on the customers array
+// of objects (each user object should just have name and age properties)
+
+//USING REDUCE
+// const arrayOfUserObj = customers.reduce( (acc, { name, age }) => {
+//     acc.push({name, age});
+//     return acc;
+// }, []);
+//
+// console.log(arrayOfUserObj);
+//
+
+const arrayOfUserObj = customers.map(({ name, age}) => {
+    return {name, age};
+});
+console.log(arrayOfUserObj);
+
+// PROBLEM 3 - create an array of civil servant customers (teachers and police officers)
+// containing the same properties as the objects on the customers objects
+
+const arrayOfCivilServants = customers.filter(({occupation}) => {
+    return (occupation.toLowerCase() === 'teacher' || occupation.toLowerCase() === 'police officer');
+}).map(({name, age, occupation}) => {
+    return {name, age, occupation: 'civil servant'};
+});
+console.log(arrayOfCivilServants);
+
+
+// PROBLEM 4 - determine the average age of customers
+
+const avgAgeOfCustomers = customers.reduce( ( acc, { age } ) => acc + age, 0 ) / customers.length;
+console.log(`Average Age ${avgAgeOfCustomers}`);
+
+//            PROBLEM 1 HINT - use .map()
+//            PROBLEM 2 HINT - use .map()
+//            PROBLEM 3 HINT - use .filter()
+//            PROBLEM 4 HINT - use .reduce()
